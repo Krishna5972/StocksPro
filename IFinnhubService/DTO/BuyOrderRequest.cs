9f0fe8a7705761f1d9d830bae2c424653a53dff1
vamsi <vamsikrishnagannamaneni@gmail.com>
2024-09-22 03:15:59 -0500

    Add IStocksService Interface Add DTO Objects

--- IFinnhubService/DTO/BuyOrderRequest.cs
+++ IFinnhubService/DTO/BuyOrderRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace ServiceContracts.DTO
 {
     public class BuyOrderRequest
     {
+        [Required(ErrorMessage = "Stock Symbol is missing")]
+        public string StockSymbol { get; set; }
+        [Required(ErrorMessage = "Stock Name is missing")]
+        public string StockName { get; set; }
+
+        [Range(typeof(DateTime), "1/1/2000", "12/12/2099",ErrorMessage = "Value for {0} must be between {1} and {2}")]
+        public DateTime DateAndTimeOfOrder { get; set; }
+        [Range(1, 100000, ErrorMessage = "Quantity must be in range of 1 and 100000")]
+        public uint Quantity { get; set; }
+        [Range(1, 100000, ErrorMessage = "Quantity must be in range of 1 and 100000")]
+        public double Price { get; set; }
     }
 }
--- IFinnhubService/DTO/BuyOrderResponse.cs
+++ IFinnhubService/DTO/BuyOrderResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ServiceContracts.DTO
 {
     public class BuyOrderResponse
     {
+
+        public Guid BuyOrderID { get; set; }
+        public string StockSymbol { get; set; }
+        public string StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+        public uint Quantity { get; set; }
+        public double Price { get; set; }
+
+        public double TradeAmount { get; set; }
     }
 }
